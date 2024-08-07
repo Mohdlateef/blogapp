3f3a00475ae9c6a207fc0c439981e9217ae8c9f7
Mohdlateef <lateefahbaba123@gmail.com>
2024-08-07 22:14:11 +0530

    error

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express=require("express");
 const authRouter = require("./routers/authRouter");
+const session=require("express-session");
+const mongoDbSession=require("connect-mongodb-session")(session);
 
-// ENV CONFIGRATION
-require("dotenv").config();
+// fileimporst
 
 const db=require("./db");
+const { collection } = require("./schemas/userSchema");
+
+
+//**constants */
+
+// ENV CONFIGRATION
 const app=express();
+require("dotenv").config();
+console.log(process.env.SECRET,"17")
+const store=new mongoDbSession({
+    uri:process.env.MONGO_URI,
+    collection:"sessions"
+})
+
+
+
 
 // globalmiddlewares
 app.use(express.json())
 app.use('/auth',authRouter)
+app.use(session({
+    secret:process.env.SECRET,
+    store:store,
+    resave:false,
+    saveUninitialized:false,
+}))
 
 // app.use({})
 // app.post("/register",(req,res)=>{
